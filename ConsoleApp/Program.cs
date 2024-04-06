f4f30366c60fc018dba477ce567a7a5f5f3381a4
s25508 <s25508@pjwstk.edu.pl>
2024-04-06 12:15:17 +0200

    Modyfikacja 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("    **   ** ");
+        Console.WriteLine("  *    *    *");
+        Console.WriteLine(" *            *");
+        Console.WriteLine(" *            *");
+        Console.WriteLine("  *          *");
+        Console.WriteLine("    *      *");
+        Console.WriteLine("      *  *");
+        Console.WriteLine("        *");
+    }
+}
