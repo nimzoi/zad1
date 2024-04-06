68ac864c6dbc043ab3a8b5894d3d48529e2db275
s25508 <s25508@pjwstk.edu.pl>
2024-04-06 12:16:32 +0200

    Modyfikacja 2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,9 +2,18 @@
 
 using System;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
+    {
+        PrintHeart();
+        Console.WriteLine();
+        PrintTree(5); // Adjust the parameter to change the size of the tree
+    }
+
+    static void PrintHeart()
     {
         Console.WriteLine("    **   ** ");
         Console.WriteLine("  *    *    *");
@@ -14,5 +23,15 @@ class Program
         Console.WriteLine("    *      *");
         Console.WriteLine("      *  *");
         Console.WriteLine("        *");
+    }
+
+    static void PrintTree(int height)
+    {
+        for (int i = 0; i < height; i++)
+        {
+            Console.WriteLine(new string(' ', height - i - 1) + new string('*', i * 2 + 1));
+        }
+
+        Console.WriteLine(new string(' ', height - 1) + "|");
     }
 }
